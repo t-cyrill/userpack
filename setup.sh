ca0efd0768344be79395b5fc2afd02d934f42443
cyrill <siril.taka@gmail.com>
2012-04-04 16:42:24 +0900

    aliasesを追加

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-cp -R config/.bash_user config/.bashrc config/.screen config/.screenrc config/.vimrc config/.zsh_user config/.zshrc ~/
+cp -R config/.bash_user config/.bashrc config/.screen config/.screenrc config/.vimrc config/.zsh_user config/.zshrc config/.aliases ~/
 chsh -s /bin/zsh
 
