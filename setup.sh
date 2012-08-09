c2907df39639aff60c045ddcf2745aa9d4ad5320
cyrill <siril.taka@gmail.com>
2012-08-09 12:35:45 +0900

    ものぐさなのでBundleInstallも自動でやらせるよう変更

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ chsh -s /bin/zsh
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp -R config/.bash_user config/.bashrc config/.screen config/.screenrc config/.vimrc config/.zsh_user config/.zshrc config/.aliases ~/
 sed -i -e "s|{HOME}|${HOME}|g" config/.screen/log
+vim -c BundleInstall! -c ":q" -c ":q"
+
