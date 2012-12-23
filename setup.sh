e8567e184b43437b2163775d7e4a6cc337d1428a
cyrill <siril.taka@gmail.com>
2012-12-24 00:24:03 +0900

    setup.shを大幅に変更

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
-chsh -s /bin/zsh
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-cp -R config/.bash_user config/.bashrc config/.screen config/.screenrc config/.vimrc config/.zsh_user config/.zshrc config/.aliases ~/
-sed -i -e "s|{HOME}|${HOME}|g" config/.screen/log
+#!/bin/sh
+
+chsh -s /bin/zsh || exit
+cp -R dotfiles ~
+sed -i -e "s|{HOME}|${HOME}|g" ~/dotfiles/.screen/log
+git clone https://github.com/gmarik/vundle.git ~/dotfiles/.vim/bundle/vundle
+
+pushd ~
+ln -s dotfiles/.shell/zsh/global/zshenv $HOME/.zshenv
+ln -s dotfiles/.vim/vimrc $HOME/.vimrc
+ln -s dotfiles/.screen/main $HOME/.screenrc
+popd
+
 vim -c BundleInstall! -c ":q" -c ":q"
+php ~/dotfiles/.vim/dic/builder.php > ~/dotfiles/.vim/dic/php.dict
+
+mkdir -p ~/dotfiles/.shell/zsh/functions/
+mkdir -p ~/dotfiles/.shell/zsh/history/
+mkdir -p ~/dotfiles/.shell/zsh/local/
+mkdir -p ~/dotfiles/.vim/syntax/
+mkdir -p ~/dotfiles/.vim/tags/
 
