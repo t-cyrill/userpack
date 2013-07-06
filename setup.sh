43063a81875787dc30253cf548f74bd533405bb5
cyrill <siril.taka@gmail.com>
2013-07-06 21:21:12 +0900

    Fix wrong directory path

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
-chsh -s /bin/zsh || exit
-ln -s dotfiles {$HOME}
-sed -i -e "s|{HOME}|${HOME}|g" ~/dotfiles/.screen/log
-git clone https://github.com/gmarik/vundle.git ~/dotfiles/.vim/bundle/vundle
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-pushd ~
-ln -s dotfiles/.shell/zsh/global/zshenv $HOME/.zshenv
-ln -s dotfiles/.vim/vimrc $HOME/.vimrc
-ln -s dotfiles/.screen/main $HOME/.screenrc
-popd
+chsh -s /bin/zsh || exit
+ln -s ${SCRIPT_DIR}/dotfiles ${HOME}
+# sed -i -e "s|{HOME}|${HOME}|g" ~/dotfiles/.screen/log
+git clone https://github.com/gmarik/vundle.git ${HOME}/dotfiles/.vim/bundle/vundle
+
+ln -s ${HOME}/dotfiles/.shell/zsh/global/zshenv ${HOME}/.zshenv
+ln -s ${HOME}/dotfiles/.vim/vimrc ${HOME}/.vimrc
+ln -s ${HOME}/dotfiles/.screen/main ${HOME}/.screenrc
 
 vim -c BundleInstall! -c ":q" -c ":q"
-php ~/dotfiles/.vim/dic/builder.php > ~/dotfiles/.vim/dic/php.dict
+php ${HOME}/dotfiles/.vim/dic/builder.php > ${HOME}/dotfiles/.vim/dic/php.dict
 
-mkdir -p ~/dotfiles/.shell/zsh/functions/
-mkdir -p ~/dotfiles/.shell/zsh/history/
-mkdir -p ~/dotfiles/.shell/zsh/local/
-mkdir -p ~/dotfiles/.vim/syntax/
-mkdir -p ~/dotfiles/.vim/tags/
+cd ${HOME}/userpack/dotfiles/.vim/bundle/vimproc
+make -f make_unix.mak
+
+mkdir -p ${HOME}/dotfiles/.shell/zsh/functions/
+mkdir -p ${HOME}/dotfiles/.shell/zsh/history/
+mkdir -p ${HOME}/dotfiles/.shell/zsh/local/
+mkdir -p ${HOME}/dotfiles/.vim/syntax/
+mkdir -p ${HOME}/dotfiles/.vim/tags/
 
