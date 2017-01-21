77a107028703c2224ab73c85c7731aa0ddb63776
cyrill <siril.taka@gmail.com>
2017-01-21 16:57:59 +0900

    Rename .vim -> vim

--- setup.sh
+++ setup.sh
@@ -18,21 +18,21 @@ fi
 
 ln -sf ${SCRIPT_DIR}/dotfiles ${HOME}
 # sed -i -e "s|{HOME}|${HOME}|g" ~/dotfiles/.screen/log
-git clone https://github.com/gmarik/vundle.git ${HOME}/dotfiles/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ${HOME}/dotfiles/vim/bundle/vundle
 
 ln -sf ${HOME}/dotfiles/.shell/zsh/global/zshenv ${HOME}/.zshenv
-ln -sf ${HOME}/dotfiles/.vim/vimrc ${HOME}/.vimrc
+ln -sf ${HOME}/dotfiles/vim/vimrc ${HOME}/.vimrc
 ln -sf ${HOME}/dotfiles/.screen/main ${HOME}/.screenrc
 
 vim -c BundleInstall! -c ":q" -c ":q"
-php ${HOME}/dotfiles/.vim/dic/builder.php > ${HOME}/dotfiles/.vim/dic/php.dict
+php ${HOME}/dotfiles/vim/dic/builder.php > ${HOME}/dotfiles/vim/dic/php.dict
 
-cd ${HOME}/userpack/dotfiles/.vim/bundle/vimproc
+cd ${HOME}/userpack/dotfiles/vim/bundle/vimproc
 make -f make_unix.mak
 
 mkdir -p ${HOME}/dotfiles/.shell/zsh/functions/
 mkdir -p ${HOME}/dotfiles/.shell/zsh/history/
 mkdir -p ${HOME}/dotfiles/.shell/zsh/local/
-mkdir -p ${HOME}/dotfiles/.vim/syntax/
-mkdir -p ${HOME}/dotfiles/.vim/tags/
+mkdir -p ${HOME}/dotfiles/vim/syntax/
+mkdir -p ${HOME}/dotfiles/vim/tags/
 
